84650a7aa75a5b23e47475220bc8c76df39e64aa
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-08-11 15:21:04 -0400

    change appId and title

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const appId = 'org.openedx.frontend.app.template';
+export const appId = 'org.openedx.frontend.app.static.pages';
 export const templateRole = 'org.openedx.frontend.role.template';
