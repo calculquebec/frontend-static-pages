d126125f9def984dfa50177a08af3eadbe68c04b
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-08-17 08:33:13 -0400

    attempt to replace header

--- src/slots.tsx
+++ src/slots.tsx
@@ -1,9 +1,13 @@
 import { SlotOperation } from '@openedx/frontend-base';
 
-import { exampleHeaderApp } from './widgets/ExampleHeader';
+//import { exampleHeaderApp } from './widgets/ExampleHeader';
+import { learnerDashboardHeaderApp } from '@openedx/frontend-app-learner-dashboard/src/widgets/LearnerDashboardHeader';
 
 const slots: SlotOperation[] = [
-  ...(exampleHeaderApp.slots ?? []),
+//    [],
+//  ...(exampleHeaderApp.slots ?? []),
+//  ...(headerApp.slots as []),
+    ...(learnerDashboardHeaderApp.slots as []),
 ];
 
 export default slots;
